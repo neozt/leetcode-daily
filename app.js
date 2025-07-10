071fc7e84ebfd9e6ea7a8e82103d176e01e907d4
Neo Zhen Ting <neozt99@gmail.com>
2025-07-10 17:30:56 +0800

    use custom domain name

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import express from "express";
 import { getDailyQuestion, constructDailyQuestionLink } from "./leetcode-client.js";
 import rateLimit from 'express-rate-limit'
 
+const BASE_URL = process.env.BASE_URL;
+
 const app = express();
 
 const limiter = rateLimit({
@@ -15,10 +17,12 @@ const limiter = rateLimit({
 
 app.use(limiter);
 
+app.set('trust proxy', 1)
+
 app.get("/", (req, res) => {
   return res.status(200).json({
     message: "LeetCode does not provide any way to directly navigate to today's Daily Question, so I did it myself. Bookmark the provided URL to be redirected to each day's Daily Question.",
-    url: "https://uvor3ogmr3.execute-api.ap-southeast-1.amazonaws.com/daily"
+    url: `${BASE_URL}/daily`
   });
 });
 
@@ -37,6 +41,7 @@ app.get("/daily", async (req, res) => {
   return res.redirect(303, dailyQuestionUrl);
 });
 
+app.get('/ip', (request, response) => response.send(request.ip))
 
 app.use((req, res) => {
   return res.status(404).json({
