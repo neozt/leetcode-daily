655cb566e648140755bb393fae484becce989d6d
Neo Zhen Ting <neozt99@gmail.com>
2025-07-10 16:20:34 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,47 @@
+import serverless from "serverless-http";
+import express from "express";
+import { getDailyQuestion, constructDailyQuestionLink } from "./leetcode-client.js";
+import rateLimit from 'express-rate-limit'
+
+const app = express();
+
+const limiter = rateLimit({
+  windowMs: 60 * 60 * 1000, // 1 hour
+  limit: 60,
+  standardHeaders: 'draft-7',
+  legacyHeaders: false,
+  message: 'Too many request from this IP, try again in 1 hour',
+});
+
+app.use(limiter);
+
+app.get("/", (req, res) => {
+  return res.status(200).json({
+    message: "LeetCode does not provide any way to directly navigate to today's Daily Question, so I did it myself. Bookmark the provided URL to be redirected to each day's Daily Question.",
+    url: "https://uvor3ogmr3.execute-api.ap-southeast-1.amazonaws.com/daily"
+  });
+});
+
+app.get("/daily", async (req, res) => {
+  const dailyQuestion = await getDailyQuestion()
+
+  const link = dailyQuestion.data.activeDailyCodingChallengeQuestion.link
+  const date = dailyQuestion.data.activeDailyCodingChallengeQuestion.date
+  if (!link) {
+    console.error(`Failed to retrieve daily question from Leetcode. link=${link}, response=${JSON.stringify(dailyQuestion)}`);
+    throw new Error("Failed to retrieve daily question from Leetcode");
+  }
+
+  const dailyQuestionUrl = constructDailyQuestionLink(link, date)
+  console.log("Redirecting to: ", dailyQuestionUrl);
+  return res.redirect(303, dailyQuestionUrl);
+});
+
+
+app.use((req, res) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+export const handler = serverless(app);
---
+++ leetcode-client.js
@@ -0,0 +1,32 @@
+import {DAILY_QUESTION_QUERY} from './leetcode-queries.js'
+import axios from 'axios'
+
+const LEETCODE_API_URL = process.env.LEETCODE_API_URL || 'https://leetcode.com/graphql';
+const LEETCODE_URL = process.env.LEETCODE_URL || 'https://leetcode.com';
+
+
+export async function getDailyQuestion() {
+    return queryLeetCodeAPI(DAILY_QUESTION_QUERY, {});
+}
+
+export function constructDailyQuestionLink(link, date) {
+    return `${LEETCODE_URL}${link}?envType=daily-question&envId=${date}`
+}
+
+async function queryLeetCodeAPI(query, variables) {
+    try {
+        const response = await axios.post(LEETCODE_API_URL, { query, variables });
+        if (response.data.errors) {
+            throw new Error(response.data.errors[0].message);
+        }
+        return response.data;
+    } catch (error) {
+        if (error.response) {
+            throw new Error(`Error from LeetCode API: ${JSON.stringify(error.response.data)}`);
+        } else if (error.request) {
+            throw new Error('No response received from LeetCode API');
+        } else {
+            throw new Error(`Error in setting up the request: ${error.message}`);
+        }
+    }
+}
