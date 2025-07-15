9f0423b97ad6cc1fd4f63f264643e4011c8af627
Neo Zhen Ting <neozt99@gmail.com>
2025-07-15 14:27:52 +0800

    upgrade to aws-sdk v3

--- src/s3-client.js
+++ src/s3-client.js
@@ -1,13 +1,18 @@
-import AWS from "aws-sdk";
+import {
+  GetObjectCommand,
+  PutObjectCommand,
+  S3Client,
+} from "@aws-sdk/client-s3";
 import NodeCache from "node-cache";
 
 import * as S3_CONSTANT from "./s3-constant.js";
+
+const s3Client = new S3Client();
 
 const POTD_CACHE_KEY = "POTD_CACHE_KEY";
 const POTD_CACHE_TTL_SECONDS = 60;
 
 const cache = new NodeCache();
-const s3 = new AWS.S3();
 
 export async function storePotdData(potdData) {
   const putObjectRequest = {
@@ -18,7 +23,7 @@ export async function storePotdData(potdData) {
 
   try {
     console.log(`Uploading to S3: ${JSON.stringify(putObjectRequest)}`);
-    const result = await s3.putObject(putObjectRequest).promise();
+    const result = await s3Client.send(new PutObjectCommand(putObjectRequest));
     console.log(`Successfully uploaded to S3: ETag=${result.ETag}`);
   } catch (e) {
     console.error(
@@ -36,13 +41,13 @@ export async function retrievePotdData() {
     return cachedResponse;
   }
 
-  const getPotdRequest = {
-    Bucket: S3_CONSTANT.BUCKET_NAME,
-    Key: S3_CONSTANT.POTD_FILE_NAME,
-  };
-
-  const s3Result = await s3.getObject(getPotdRequest).promise();
-  const potd = await JSON.parse(s3Result.Body.toString());
+  const result = await s3Client.send(
+    new GetObjectCommand({
+      Bucket: S3_CONSTANT.BUCKET_NAME,
+      Key: S3_CONSTANT.POTD_FILE_NAME,
+    }),
+  );
+  const potd = await JSON.parse(await result.Body.transformToString("utf-8"));
 
   if (potd) {
     cache.set(POTD_CACHE_KEY, potd, POTD_CACHE_TTL_SECONDS);
