b6468bd178a3bbbfbcab939eecc367215c1123cb
Neo Zhen Ting <neozt99@gmail.com>
2025-07-17 20:04:14 +0800

    moved github url into env

--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -2,6 +2,7 @@ import { RequestHandler } from "express";
 import { retrievePotd } from "../helpers/retrieve-potd";
 
 const BASE_URL = process.env.BASE_URL;
+const GITHUB_URL = process.env.GITHUB_URL;
 
 export const redirectToDailyQuestion: RequestHandler = async (req, res) => {
   const potdData = await retrievePotd();
@@ -18,8 +19,8 @@ export const displayInfo: RequestHandler = (req, res) => {
   return res.status(200).json({
     message:
       "LeetCode does not provide any way to directly navigate to today's Daily Question, so I did it myself. Bookmark the provided URL to be redirected to each day's Daily Question.",
-    url: `${BASE_URL}/`,
-    github: "https://github.com/neozt/leetcode-daily",
+    url: BASE_URL,
+    github: GITHUB_URL,
   });
 };
 
